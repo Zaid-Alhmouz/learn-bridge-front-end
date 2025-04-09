6f235acd8f957d87e64e63ef338a68d2d90a2a64
Tamer <tamerse21business@gmail.com>
2025-04-09 20:27:40 +0300

    New UI Pages: Create-Post and Instructor Bio Design

---
+++ learn-bridge-front-end/src/app/components/Instructor-bio/instructor-bio.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+// I don't understand this talasem: Tamer :)
+@Component({
+  selector: 'app-instructor-bio',
+  templateUrl: './instructor-bio.html',
+  styleUrl: './instructor-bio.scss',
+})
+export class InstructorBio {}
