d8fc5e184f58c734dc2a4d0072731206e7b43788
faisalalbarq <faisalalbarq09@gmail.com>
2025-05-03 18:58:02 +0300

    Nothings

--- learn-bridge-front-end/src/app/components/find-instructor/find-instructor.component.ts
+++ learn-bridge-front-end/src/app/components/find-instructor/find-instructor.component.ts
@@ -14,16 +14,17 @@ export class FindInstructorComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.fetchInstructors(); // load all instructors by default
+    this.fetchInstructors(); // رح يشتغل اول ما الصفحه تفتح
   }
 
   fetchInstructors() {
+
     // const params: any = {};
     // if (this.searchKeyword.trim()) {
     //   params.search = this.searchKeyword.trim();
     // }
 
-    // this.http.get<any>('https://your-api-base-url.com/instructors', { params })
+    // this.http.get<any>('https:// .... /instructors', { params })
     //   .subscribe({
     //     next: (response) => {
     //       this.instructors = response.data;
